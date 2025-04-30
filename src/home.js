36f56e2df629dc446ee6f0abf6da4ed0d3ea2cea
Phoesston <joshua.jr2017@gmail.com>
2025-04-29 22:12:21 -0500

    tabs finished just need deployment

--- src/home.js
+++ src/home.js
@@ -1,10 +1,10 @@
 import filipinoHut from './assets/images/filipinoHut.jpg'
 
-export function loadPage(){
-    const contentDiv = document.getElementById('content');
+export function loadHome(){
+    const contentDiv = document.createElement('div');
 
     const title = document.createElement('h1');
-    title.textContent = 'Welcome to Chica Hut';
+    title.textContent = 'Welcome to Coco Hut';
     contentDiv.appendChild(title);
 
     const img = document.createElement('img');
@@ -13,7 +13,7 @@ export function loadPage(){
     contentDiv.appendChild(img);
 
     const description = document.createElement('p');
-    description.textContent = 'At Chica Hut, we serve authentic Filipino dishes with fresh, local ingredients.';
+    description.textContent = 'At Coco Hut, we serve authentic Filipino dishes with fresh, local ingredients.';
     contentDiv.appendChild(description);
 
     const hours = document.createElement('div');
@@ -33,5 +33,7 @@ export function loadPage(){
         <p>204 W Shell Point Rd<br>Ruskin, FL 33570</p>
     `;
     contentDiv.appendChild(address);
+
+    return contentDiv;
 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import {loadPage} from './loadPage';
+import {loadHome} from './home';
+import {loadMenu} from './menu';
+import {loadAbout} from './about';
 import "./styles.css";
 
-loadPage();
+function setActiveTab(tab){
+    const content = document.getElementById('content');
+    content.innerHTML = ''; //clears the current content
+    content.appendChild(tab());
+}
 
-console.log("Page and content added")
+function initWebsite(){
+    const buttons = document.querySelectorAll('nav button');
+
+    buttons[0].addEventListener('click', ()=> setActiveTab(loadHome));
+    buttons[1].addEventListener('click', ()=> setActiveTab(loadMenu));
+    buttons[2].addEventListener('click', ()=> setActiveTab(loadAbout));
+
+    setActiveTab(loadHome);
+}
+
+initWebsite();
